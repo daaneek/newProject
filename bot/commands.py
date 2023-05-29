6b96a957672528f8a40998717be9ea15ec846037
NBuhsx <trubitsyn98@gmail.com>
2023-05-29 23:25:15 +0300

    formating command

--- bot/commands.py
+++ bot/commands.py
@@ -6,13 +6,14 @@ from aiogram.dispatcher.storage import FSMContext
 
 from bot.dictionary import start_menu, dictionary_button, ListDictionaries
 
-async def start(message: types.Message, state:FSMContext):
+
+async def start(message: types.Message, state: FSMContext):
     """ Запуск бота """
     await message.answer(f"Привет {message.from_user.first_name}", reply_markup=start_menu)
 
 
 async def menu_dictionary(
-    message: types.Message, list_dictionaries: ListDictionaries):
+        message: types.Message, list_dictionaries: ListDictionaries):
     """ Словари """
     await message.answer(
         f"{message.from_user.first_name} Выбери словарь: ",
@@ -20,25 +21,30 @@ async def menu_dictionary(
 
 
 async def show_dictionary(
-    call: types.CallbackQuery, list_dictionaries: ListDictionaries):
+        call: types.CallbackQuery, list_dictionaries: ListDictionaries):
     _, category = call.data.split(":")
     text = list_dictionaries.get_by_name(category).get()
     await call.message.edit_text(
         f"Словарь категории {category}\n {text}")
 
-### Свалка 
+
 async def shit(message: types.Message):
-    await message.reply(f'Нажмите /start', parse_mode=types.ParseMode.MARKDOWN)
+    """ Свалка """
+    await message.reply('Нажмите /start', parse_mode=types.ParseMode.MARKDOWN)
 
 
 def register_commands(dp: Dispatcher):
     """ Регистратор команд """
     # Стартовое меню
-    dp.register_message_handler(start, CommandStart(deep_link=re.compile('\d+')) | Command("start"), state='*')   
-    
+    dp.register_message_handler(start, CommandStart(
+        deep_link=re.compile(r'\d+')) | Command("start"), state='*')
+
     # Меню словарей
-    dp.register_message_handler(menu_dictionary, Text(equals="📊 Словари") | Command('dict'))
+    dp.register_message_handler(menu_dictionary, Text(
+        equals="📊 Словари") | Command('dict'))
     # Словарь
-    dp.register_callback_query_handler(show_dictionary, dictionary_button.filter())
-    # Мусор 
-    dp.register_message_handler(shit, content_types=types.ContentType.ANY, state='*')   
+    dp.register_callback_query_handler(
+        show_dictionary, dictionary_button.filter())
+    # Мусор
+    dp.register_message_handler(
+        shit, content_types=types.ContentType.ANY, state='*')
